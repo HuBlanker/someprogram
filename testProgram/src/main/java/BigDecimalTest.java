7cee6bbc297173360a3d987901f4a7291c55b594
hublanker <pfliu@mobvoi.com>
2018-08-20 20:48:18 +0800

    export excel into mysql

---
+++ testProgram/src/main/java/BigDecimalTest.java
@@ -0,0 +1,22 @@
+// Copyright 2018 Mobvoi Inc. All Rights Reserved.
+
+import java.math.BigDecimal;
+
+/**
+ * created by pfliu on 18-8-21
+ */
+public class BigDecimalTest {
+
+  public static void  main(String [] args){
+
+    System.out.println(new BigDecimal("15").divide(new BigDecimal("5")));
+
+
+
+
+
+  }
+
+
+
+}
