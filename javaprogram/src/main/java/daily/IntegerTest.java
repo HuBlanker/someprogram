140f1ef1809a3324f03746907681475fd28baa9b
liupanfeng <liupanfeng@kanzhun.com>
2019-05-13 00:00:02 +0800

    x

---
+++ javaprogram/src/main/java/daily/IntegerTest.java
@@ -0,0 +1,22 @@
+package daily;
+
+/**
+ * Created by pfliu on 2019/05/12.
+ */
+public class IntegerTest {
+
+
+    public static void main(String[] args) {
+        Integer i  = 12;
+        Integer i1  = 12;
+
+        Integer i2  = 129;
+        Integer i3  = 129;
+
+        Integer.valueOf()
+
+        System.out.println(i == i1);
+        System.out.println(i2 == i3);
+
+    }
+}
